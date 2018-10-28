42d668a1d2a2eaf0a5eba1f8e4830e34484259b9
Sruuujaaan <32270370+Sruuujaaan@users.noreply.github.com>
2018-10-27 21:01:46 -0400

    # Policies

--- analysis.py
+++ analysis.py
@@ -30,39 +30,56 @@ def question2():
    return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+        Prefer the close exit (+1), risking the cliff (-10). Kept noise at 0 to maximize the output. started discount factor at 0.2.
+        Two steps to reach +1 exit. East East and North. Thus set discount factor 0.2 and living reward 0.4.
+    """
+    answerDiscount = 0.2
+    answerNoise = 0
+    answerLivingReward = 0.4
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+        Prefer the close exit (+1), but avoiding the cliff (-10). Assuming there is no living reward, assume movement to be certain.
+        works for discount 0.1/0.2/0.3 and noise 0.1/0.2/0.3 and reward 0.
+    """
+    answerDiscount = 0.2
+    answerNoise = 0.1
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+        Prefer the close exit (+1), but avoiding the cliff (-10). Assuming there is no living reward, assume movement to be certain.
+        works for discount 0.1/0.2/0.3 and noise 0.1/0.2/0.3 and reward 0.
+    """
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 0.9
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+        Prefer the distant exit (+10), avoiding the cliff (-10)
+        I choose 0.9 discount factor as the higher the discount factor, the farther our rewards will propagate through time.
+        0.1 as noise as lesser noise is preferrable and 0.6 as more reward is preferrable, but more living reward may make
+        agent lazy and greedy, so I choose 0.6 for living reward
+    """
+    answerDiscount = 0.9
+    answerNoise = 0.1
+    answerLivingReward = 0.6
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
-
+   
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """
+        Avoid both exits and the cliff (so an episode should never terminate)
+        Works for discount 1 and noise 1 and reward 1.
+    """
+    answerDiscount = 1
+    answerNoise = 1
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question6():
     answerEpsilon = None
